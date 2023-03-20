1dbd2fdf43674ae53fa6c9b7691428e3099ccc45
allessander <alex4turbin@gmail.com>
2023-03-20 18:07:14 +0300

    Исправил методы в классе ArrayStorage (1)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,15 +1,13 @@
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
+
 public class ArrayStorage {
-    private final int MAX_STORAGE_VALUE = 10_000;
-    Resume[] storage = new Resume[MAX_STORAGE_VALUE];
-    int size = 0;
+    private final int capacity = 10_000;
+    Resume[] storage = new Resume[capacity];
+    int size;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
@@ -19,47 +17,40 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        int resumeIndex = findResume(uuid);
-        if (resumeIndex != -1) {
-            return storage[resumeIndex];
-        }
-        return null;
+        int resumeIndex = findIndex(uuid);
+        return resumeIndex != -1 ? storage[resumeIndex] : null;
     }
 
     void delete(String uuid) {
-        int resumeIndex = findResume(uuid);
+        int resumeIndex = findIndex(uuid);
         if (resumeIndex != -1) {
             storage[resumeIndex] = null;
+            moveLeft(resumeIndex);
+            size--;
         }
-        moveLeft(resumeIndex);
-        size--;
-
     }
 
     void moveLeft(int index) {
-        for (int i = index; i < size(); i++) {
+        for (int i = index; i < size; i++) {
             storage[i] = storage[i + 1];
         }
     }
 
-    int findResume(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) return i;
-        }
-        return -1;
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-
     Resume[] getAll() {
-        Resume[] result = Arrays.copyOf(storage, size());
-        return result;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
         return size;
+    }
+
+    private int findIndex(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
