3fa96aff93ee04da2e45a059666f8bdb292c2819
allessander <alex4turbin@gmail.com>
2023-03-21 22:53:54 +0300

    Исправил методы findIndex() и delete(). Удалил метод moveLeft()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -24,15 +24,9 @@ public class ArrayStorage {
     void delete(String uuid) {
         int resumeIndex = findIndex(uuid);
         if (resumeIndex != -1) {
-            storage[resumeIndex] = null;
-            moveLeft(resumeIndex);
+            storage[resumeIndex] = storage[size - 1];
+            storage[size - 1] = null;
             size--;
-        }
-    }
-
-    void moveLeft(int index) {
-        for (int i = index; i < size; i++) {
-            storage[i] = storage[i + 1];
         }
     }
 
@@ -45,12 +39,11 @@ public class ArrayStorage {
     }
 
     private int findIndex(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
                 return i;
             }
         }
         return -1;
     }
-
 }
